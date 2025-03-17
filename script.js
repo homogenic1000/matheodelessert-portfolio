de3b0069fb17900117d25667a3089d0510fbc324
Mathéo Delessert <hello@matheodelssert.ch>
2025-03-17 10:49:57 +0100

    trying to add interactive aniamtion to intro paragraph, need to continue the script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+let FallIntro = document.getElementById("introduction");
+
+function displayFallIntro() {
+    let prepareText = FallIntro.innerText;
+
+    for (let i = 0; i < prepareText.length; i++) {
+        const char = prepareText[i]
+        const span = document.createElement("span");
+        span.className = "letter"
+        span.innerText = char;
+        prepareText.appendChild(span)
+        letter.addEventListener("mouseenter", () => {
+            const randomX = Math.random() * 30 - 10;
+            const randomY = Math.random() * 20 -10;
+            const randomDEG = Math.random() * Math.PI * 20 - 10;
+            letter.style.transform = `translate(${randomX},${randomY})`;
+        })
+
+    }
+
+    displayFallIntro();
+
+
+}
+
 
