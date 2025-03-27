d1230d2719ebc8c68437984875ba7179e67d5371
Mathéo Delessert <hello@matheodelssert.ch>
2025-03-27 15:43:15 +0100

    adding dark mode
    need to change the logo for dark mode

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ window.addEventListener("scroll", () => {
     let valueR = window.scrollY % 360;
     logo.style.transform = `rotate(${valueR}deg)`;
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    let darkMode = document.getElementById("dark-mode");
+    if (darkMode) {
+        darkMode.addEventListener("click", () => {
+            document.body.classList.toggle("dark-mode");
+            document.button.classList.toggle("dark-mode");
+            console.log("dark-mode");
+        });
+    } else {
+        console.error("L'élément #dark-mode est introuvable !");
+    }
+
+    });
+
